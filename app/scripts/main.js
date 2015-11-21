827f617f21c6e8fda11f4fc64a9264e94877a76d
John Hagler <haglerjo@gmail.com>
2015-11-20 16:47:25 -0800

    first round at interface only

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,4 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+$(function(){
+	$('#add-through').click(addThroughPoint);
+	$('#search-form').submit(submitSearchForm);
+});
---
+++ app/scripts/second.js
@@ -0,0 +1,35 @@
+function addThroughPoint(e) {
+	e.preventDefault();
+	
+	var new_through = $.parseHTML($('#through-template').html());
+	
+	$('#throughs').append(new_through);
+	$(new_through).find('input').focus();
+	$(new_through).find('a.remove').click(function(e){
+		e.preventDefault();
+		$(this).parents('.form-group').remove();
+	});
+
+	return false;
+}
+
+function submitSearchForm(e) {
+	e.preventDefault();
+
+	var start = $('#start').val(),
+		throughs = getThroughPoints(),
+		end = $('#end').val();
+
+	console.log(start + ', ' + throughs + ', ' + end);
+}
+
+function getThroughPoints() {
+	var throughs = [];
+	$('.through').each(function(){
+		var through = $(this).val();		
+		if (through !== '') {
+			throughs.push({location:through});
+		}
+	});
+	return throughs;
+}
