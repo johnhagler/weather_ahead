acb0db7907d17430fbf8d0afd2677d15bf41ad55
John Hagler <haglerjo@gmail.com>
2016-01-04 09:18:31 -0800

    update builds

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+echo 'testing build script'
+
+#ssh -p 18765 weathe73@weather-ahead.com 'cd public_html; rm -rf test; mkdir test'
+#scp -P 18765 -r dist weathe73@weather-ahead.com:public_html/test
+#scp -P 18765 -r bower_components/weather-icons/font weathe73@weather-ahead.com:public_html/test/fonts
+
+
+
