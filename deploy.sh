9ef001126834996fd3c8f153c498ee6f0d5c3565
John Hagler <haglerjo@gmail.com>
2016-01-04 10:31:03 -0800

    update build

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 echo 'testing build script'
+ls -lah
 
 #ssh -p 18765 weathe73@weather-ahead.com 'cd public_html; rm -rf test; mkdir test'
 #scp -P 18765 -r dist weathe73@weather-ahead.com:public_html/test
